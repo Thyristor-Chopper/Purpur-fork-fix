f68a0d8f17bfe47cfb961aa2859c74dbc74eb571
싸이리스터 초퍼 <gec_thyristor_chopper@[127.0.0.1]>
2026-02-26 01:58:05 +0900

    fix repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
+    maven("https://repo.papermc.io/repository/maven-public/") {
         content {
             onlyForConfigurations(PAPERCLIP_CONFIG)
         }
@@ -50,7 +50,7 @@ subprojects {
     repositories {
         mavenCentral()
         maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://ci.emc.gs/nexus/content/groups/aikar/")
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
@@ -66,6 +66,8 @@ paperweight {
     decompileRepo.set("https://files.minecraftforge.net/maven/")
 
     usePaperUpstream(providers.gradleProperty("paperCommit")) {
+        url.set(github("Thyristor-Chopper", "PaperSpigot-fork-fix"))
+
         withPaperPatcher {
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
             apiOutputDir.set(layout.projectDirectory.dir("Purpur-API"))
